16de4a3ab9d225656d1d3ff9963d0f0863c1a96a
rolandomajait1-rgb <rolandomajait1@gmail.com>
2025-10-19 15:40:04 +0800

    DONE MIDTERM

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: "/appdev1-midterm-exam1/",
+    plugins: [react()],
+
+});
